1026e2cd9febbf857b8377393e32d10b36b9d15e
MauricioL88 <lisboa.sse@gmail.com>
2025-06-11 21:11:42 -0300

    Listando os tipos de dados em Dart

--- fundamentos_dart/bin/fundamentos_dart.dart
+++ fundamentos_dart/bin/fundamentos_dart.dart
@@ -3,3 +3,16 @@ import 'package:fundamentos_dart/fundamentos_dart.dart' as fundamentos_dart;
 void main(List<String> arguments) {
   print('Hello world: ${fundamentos_dart.calculate()}!');
 }
+
+/*
+  Tipo de dados no Dart\
+  var -> mais genérico
+  const -> constantes
+  int
+  double
+  bool
+  String
+  Dynamic
+  List ou List<>
+  Map ou Map<>
+*/
